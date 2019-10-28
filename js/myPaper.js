5d82ce119a3f589384c34efb2b7e057b734135fb
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-10-28 16:37:21 +0100

    Can make an another line using same variable

--- js/myPaper.js
+++ js/myPaper.js
@@ -5,4 +5,14 @@ var start = new Point(100, 100);
 
 path.moveTo(start);
 
-path.lineTo(start + [100, -50]);
+path.lineTo(start + [500, -200]);
+
+
+var path = new Path();
+
+path.strokeColor = 'red';
+var start = new Point(100, 100); 
+
+path.moveTo(start);
+
+path.lineTo(start + [500, -100]);
