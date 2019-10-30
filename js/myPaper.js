c0dfdd3eb1d639b3fdeba14d0bac86679bc5e06b
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-10-30 19:47:10 +0100

    Tested by creating a circle

--- js/myPaper.js
+++ js/myPaper.js
@@ -16,3 +16,14 @@ var start = new Point(100, 100);
 path.moveTo(start);
 
 path.lineTo(start + [500, -100]);
+
+var path = new Path.Circle({
+	center: view.center,
+	radius: 30,
+	strokeColor: 'black'
+});
+
+function onResize(event) {
+	// Whenever the window is resized, recenter the path:
+	path.position = view.center;
+}
