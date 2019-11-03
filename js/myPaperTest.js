c661fa65db7c6d8692a8d529fab01f033f628ba8
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-11-03 07:18:20 +0100

    Experiment with circle

--- js/myPaperTest.js
+++ js/myPaperTest.js
@@ -17,19 +17,20 @@ var startRed = new Point(100, 100);
 redLine.moveTo(startRed);
 
 redLine.lineTo(startRed + [500, -100]);
-/*
+
 // Circle
-var path = new Path.Circle({
+var blackCircle = new Path.Circle({
 	center: view.center,
 	radius: 30,
-	strokeColor: 'black'
+	strokeColor: 'black',
+	fillColor: 'orange'
 });
 
 function onResize(event) {
 	// Whenever the window is resized, recenter the path:
 	path.position = view.center;
 }
-
+/*
 // Square
 var path = new Path.square({
 	center:
